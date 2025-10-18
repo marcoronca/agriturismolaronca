a5a420d9355ec0bcaf5485e733bf6303ca9512dd
marcoronca <marco.roncareggi@gmail.com>
2025-10-18 15:47:25 +0200

    Update Footer.tsx

--- src/app/components/layout/Footer.tsx
+++ src/app/components/layout/Footer.tsx
@@ -60,6 +60,7 @@ export default function Footer({ globalContents, lang }: FooterProps) {
               viewBox="0 0 24 24"
               aria-hidden="true"
               className="size-6"
+              xmlns="http://www.w3.org/2000/svg"
             >
               <path d="M4 5a1 1 0 0 1 1-1h4a1 1 0 0 1 1 1v2a1 1 0 0 1-1 1H5a1 1 0 0 1-1-1V5Zm16 14a1 1 0 0 1-1 1h-4a1 1 0 0 1-1-1v-2a1 1 0 0 1 1-1h4a1 1 0 0 1 1 1v2ZM4 13a1 1 0 0 1 1-1h4a1 1 0 0 1 1 1v6a1 1 0 0 1-1 1H5a1 1 0 0 1-1-1v-6Zm16-2a1 1 0 0 1-1 1h-4a1 1 0 0 1-1-1V5a1 1 0 0 1 1-1h4a1 1 0 0 1 1 1v6Z"/> </path>
             </svg>
